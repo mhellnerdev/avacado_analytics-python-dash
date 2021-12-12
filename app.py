1849f3ebc23e4c1adb8a876afd314e045561e8ad
mhellnerdev@gmail.com <mhellnerdev@gmail.com>
2021-12-12 01:33:37 -0500

    dash working

--- app.py
+++ app.py
@@ -5,31 +5,31 @@ from dash import dcc
 from dash import html
 import pandas as pd
 
-data = pd.read_csv("avacado.csv")
+data = pd.read_csv("avocado.csv")
 data = data.query("type == 'conventional' and region == 'Albany'")
-data = ["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
+data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
 data.sort_values("Date", inplace=True)
 
 app = Dash(__name__)
 
 app.layout = html.Div(
     children=[
-        html.H1(children="Avacado Analytics",),
+        html.H1(children="Avocado Analytics",),
         html.P(
-            children="Analyze the behavior of avacado prices"
-            " and the number of avacados sold in the US"
+            children="Analyze the behavior of avocado prices"
+            " and the number of avocados sold in the US"
             " between 2015 and 2018",
         ),
         dcc.Graph(
             figure={
                 "data": [
                     {
-                        "x": data ["Date"],
+                        "x": data["Date"],
                         "y": data["AveragePrice"],
                         "type": "lines",
                     },
                 ],
-                "layout": {"title": "Average Price of Avacados"},
+                "layout": {"title": "Average Price of Avocados"},
             },
         ),
         dcc.Graph(
@@ -41,10 +41,12 @@ app.layout = html.Div(
                         "type": "lines",
                     },
                 ],
-                "layout": {"title": "Avacados Sold"},
+                "layout": {"title": "Avocados Sold"},
             },
         ),
 
     ]
 )
 
+if __name__ == "__main__":
+    app.run_server(debug=True)
